db593ef7d0800b902c7a57d934e04aac92cbafa3
soualexandre <alexandreifto2@gmail.com>
2024-01-24 12:06:52 -0300

    fix_metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,17 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Portifólio de Alexandre Souza",
+  title: "Alexandre Souza | Desenvolvedor Full Stack",
   description: "Compartilho minha jornada profissional através deste modesto portfólio. Aqui, busco destacar as experiências que moldaram quem sou, as aprendizagens que conquistei e os projetos nos quais me envolvi. Este é mais do que um simples resumo; é um convite para conhecer as diferentes facetas da minha trajetória, com suas vitórias e desafios. ",
+  keywords: ["Alexandre Souza", "Desenvolvedor", "Programador", "JavaScript", "Portifólio", "Next", "Santos", "Alexandre", "Xande"],
+  themeColor: "#000",
+  icons: [
+    {
+      src: '/assets/images/home.png',
+      sizes: 'any',
+      type: 'image/png',
+    },
+  ],
 };
 
 export default function RootLayout({
