b8a3629e85ff85766348b3ec5204c99f391e2d6b
soualexandre <alexandreifto2@gmail.com>
2024-01-24 12:13:25 -0300

    fix_metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,14 +9,6 @@ export const metadata: Metadata = {
   title: "Alexandre Souza | Desenvolvedor Full Stack",
   description: "Compartilho minha jornada profissional através deste modesto portfólio. Aqui, busco destacar as experiências que moldaram quem sou, as aprendizagens que conquistei e os projetos nos quais me envolvi. Este é mais do que um simples resumo; é um convite para conhecer as diferentes facetas da minha trajetória, com suas vitórias e desafios. ",
   keywords: ["Alexandre Souza", "Desenvolvedor", "Programador", "JavaScript", "Portifólio", "Next", "Santos", "Alexandre", "Xande"],
-  themeColor: "#000",
-  icons: [
-    {
-      src: '/assets/images/home.png',
-      sizes: 'any',
-      type: 'image/png',
-    },
-  ],
 };
 
 export default function RootLayout({
@@ -25,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="theme-color" content="#010114" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
