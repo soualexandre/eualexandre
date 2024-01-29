12f07c447756ce2697888353820797eb8833d9d4
soualexandre <alexandreifto2@gmail.com>
2024-01-29 18:57:21 -0300

    fix:projectjs_component

--- src/components/Projects/Index.tsx
+++ src/components/Projects/Index.tsx
@@ -1,29 +1,49 @@
 import Eproc from "@/assets/images/eproc.jpeg"
 import Image from "next/image"
 
-export default function Projects() {
-    return (
-        <div className="text-white mx-auto h-screen max-w-7xl">
-            <div className="py-20 sm:px-4">
-                <div className="py-5" >
-                    <h1 className="text-2xl font-bold">Projetos</h1>
-                </div>
-                <div className="max-w-sm rounded overflow-hidden shadow-lg bg-gray-900">
-                    {/* <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> */}
-                    <div className="px-6 py-4">
-                        <div>
-                        <h1>Sistema Eproc - Tribunal de Justiça do Tocantins </h1>
-                        </div>
-                        <div className="px-6 py-4">
-                            <Image src={Eproc} alt="Sistema Eproc" />
-                        </div>
-                    </div>
-                    <div className="px-6 pt-4 pb-2 text-center bg-gray-600">
-                        Share
-                    </div>
-                </div>
 
+export default function Projects() {
+    const dataProjects = [
+        {
+            "project_name": "Eproc"
+        },
+        {
+            "project_name": "Bazza Carga"
+        },
+        {
+            "project_name": "Tapetes São Carlos B2B"
+        },
+        {
+            "project_name": "Tapetes São Carlos B2B"
+        },
+        {
+            "project_name": "Tapetes São Carlos B2B"
+        }
+
+    ]
+    return (
+        <div className="text-white mx-auto max-w-7xl">
+            <div className="pt-20 pb-10 ">
+                <h1 className="text-2xl font-bold">Projetos</h1>
+            </div>
+            <div className="flex gap-4 md:gap-14 flex-wrap justify-center md:justify-normal">
+                {dataProjects.map((project) => (
+                    <div key={project.project_name} className="flex-grow max-w-sm rounded overflow-hidden shadow-lg bg-gray-900">
+                        <div className="px-6 py-4">
+                            <div>
+                                <h1>{project.project_name}</h1>
+                            </div>
+                            <div className="px-6 py-4">
+                                <Image src={Eproc} alt="Sistema Eproc" />
+                            </div>
+                        </div>
+                        <div className="px-6 pt-4 pb-2 text-center bg-gray-600">
+                            Share
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
+
     )
 }
